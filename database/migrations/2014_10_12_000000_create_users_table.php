eb97c26892bb0b853bebf59141c0e807a6e26710
Zeshan Wajid <zeshan.wajid@crowdproperty.com>
2019-12-08 11:47:33 +0000

    update users table migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('facebook_token', 1000)->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
