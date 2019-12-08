6313196f360d8e4094733c70042488c81a9c4d0c
Zeshan Wajid <zeshan.wajid@crowdproperty.com>
2019-12-08 11:59:24 +0000

    add facebook redirect login route and callback route

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/login/facebook', 'Auth\LoginController@redirectToProvider');
+Route::get('/callback', 'Auth\LoginController@handleProviderCallback');
